c9e2636a071ecadea2110ed4eda3b71134f5bdd3
Tom Copeland <tom@infoether.com>
2009-07-02 11:23:25 -0400

    Fixed controller file name.  When you destroy a NewsByte the forum should go away too.

--- app/models/news_byte.rb
+++ app/models/news_byte.rb
@@ -1,7 +1,7 @@
 class NewsByte < ActiveRecord::Base
   
   belongs_to :group
-  belongs_to :forum_group, :foreign_key => 'forum_id'
+  belongs_to :forum_group, :foreign_key => 'forum_id', :dependent => :destroy
   belongs_to :submitted_by, :class_name => 'User', :foreign_key => 'submitted_by'
   
 end
