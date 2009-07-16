566ea53aa98cb7f5a6b0ab35a74d307613227274
Tom Copeland <tom@infoether.com>
2009-07-16 11:50:52 -0400

    Switching deployment to git

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,40 +1,27 @@
 set :application, "api"
-set :repository,  "file:///var/svn/#{application}/trunk/"
-set :local_repository, "svn+ssh://rubyforge.org/var/svn/#{application}/trunk"
 
+set :scm, :git
+set :repository,  "git://rubyforge.org/api.git"
+set :repository_cache, "git_cache"
+set :deploy_via, :remote_cache
+set :git_shallow_clone, 1
 set :deploy_to, "/var/www/gforge-projects/api/"
-set :copy_strategy, :export
+
+set :use_sudo, false
 set :user,  "tom"
 set :group, "api"
 set :runner, "tom"
-set :use_sudo, false
 
 server "rubyforge.org", :app, :web
 role :db, "rubyforge.org", :primary => true
-
-def run_remote_rake(rake_cmd)
-  run "cd #{current_path} && /usr/local/bin/rake RAILS_ENV=production #{rake_cmd}"
-end
-
-namespace :deploy do
-  desc "Restarting mod_rails with restart.txt"
-  task :restart, :roles => :app, :except => { :no_release => true } do
-    run "touch #{current_path}/tmp/restart.txt"
-  end
-
-  [:start, :stop].each do |t|
-    desc "#{t} task is a no-op with mod_rails"
-    task t, :roles => :app do ; end
-  end
-end
 
 task :move_in_database_yml, :roles => :app do
   run "cp #{deploy_to}/shared/system/database.yml #{current_path}/config/"
 end
 
-after "deploy:symlink", "move_in_database_yml"
 #after "deploy", "deploy:migrate"
-#after "deploy", "deploy:cleanup"
+after "deploy:symlink", "move_in_database_yml"
+after "deploy", "deploy:cleanup"
 
 desc "Tail the production log"
 task :tail, :roles => :app do
