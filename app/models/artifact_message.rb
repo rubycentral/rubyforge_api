4e585a4f8b2b1159768d75063730411263219f4d
Tom Copeland <tom@infoether.com>
2009-08-07 02:08:04 -0400

    Added an ArtifactMessage model.

---
+++ app/models/artifact_message.rb
@@ -0,0 +1,21 @@
+# == Schema Information
+# Schema version: 20090605012055
+#
+# Table name: artifact_message
+#
+#  id           :integer         not null, primary key
+#  artifact_id  :integer         not null
+#  submitted_by :integer         not null
+#  from_email   :text            not null
+#  adddate      :integer         default(0), not null
+#  body         :text            not null
+#
+
+class ArtifactMessage < ActiveRecord::Base
+
+  set_table_name 'artifact_message'
+  
+  #belongs_to :artifact
+  belongs_to :submitted_by, :class_name => 'User', :foreign_key => 'submitted_by'
+  
+end
---
+++ test/unit/artifact_message_test.rb
@@ -0,0 +1,8 @@
+require 'test_helper'
+
+class ArtifactMessageTest < ActiveSupport::TestCase
+
+  test "syntax" do
+    ArtifactMessage.new
+  end
+end
