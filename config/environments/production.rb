ace827a4394b8e1dadf9dbafffb364f8a00b614e
Tom Copeland <tom@infoether.com>
2009-08-18 11:45:33 -0400

    using transami, not 7rans

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,6 +29,6 @@ config.action_view.cache_template_loading            = true
 
 GFORGE_WWW_FILE_DIRECTORY = "/var/www/gforge-files/"
 
-BETA_USER_LIST = %w{tom chadfowler ahoward dblack rich 7rans drbrain zenspider}
+BETA_USER_LIST = %w{tom chadfowler ahoward dblack rich drbrain zenspider transami}
 
 
