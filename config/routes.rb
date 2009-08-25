1bf4280df37908e0f81a5f39e97e0a401602963f
Tom Copeland <tom@infoether.com>
2009-08-25 13:26:18 -0400

    Nicer routes thanks to Rich

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,9 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :groups do |groups_map|
     groups_map.resources :news_bytes
     groups_map.resources :packages do |packages_map|
-      packages_map.resources :releases do |releases_map|
-        releases_map.resources :files
-      end
+      packages_map.resources :releases, :has_many => [:files]
     end
   end
-  map.resources :users do |users_map|
-    users_map.resources :groups
-  end
+  map.resources :users, :has_many => [:groups]
   map.resources :processors
 end
