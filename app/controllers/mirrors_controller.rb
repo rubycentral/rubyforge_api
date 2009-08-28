65e1feef0e8a248fcd42508215de2e1e2e03e1af
Tom Copeland <tom@infoether.com>
2009-08-28 13:54:00 -0400

    Add admin name to exclusion list

--- app/controllers/mirrors_controller.rb
+++ app/controllers/mirrors_controller.rb
@@ -4,7 +4,7 @@ class MirrorsController < ApplicationController
     respond_to do |wants| 
       wants.js do
         # Is there a nicer way to do to_json with collections and :except?
-        render :text => "[ %s ]" % Mirror.enabled.collect {|m| m.to_json(:except => [:id, :administrator_email, :enabled])}.join(","),:content_type => 'application/json' 
+        render :text => "[ %s ]" % Mirror.enabled.collect {|m| m.to_json(:except => [:id, :administrator_name, :administrator_email, :enabled])}.join(","),:content_type => 'application/json' 
       end
     end
   end
