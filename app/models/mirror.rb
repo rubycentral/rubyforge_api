a5f8c30552b6a6533e8862746051f7afc79bff1f
Tom Copeland <tom@infoether.com>
2009-09-29 15:36:03 -0400

    Some named scopes for easier mirror type lookups

--- app/models/mirror.rb
+++ app/models/mirror.rb
@@ -1,6 +1,8 @@
 class Mirror < ActiveRecord::Base
 
   named_scope :enabled, :conditions => {:enabled => true}
+  named_scope :serves_files, :conditions => {:serves_files => true}
+  named_scope :serves_gems, :conditions => {:serves_files => false}
   
   validates_format_of :url, :with => /^http/
   
