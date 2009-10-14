d8d17d18c90d96bcf28c0e44cca8c43660564f92
Tom Copeland <tom@infoether.com>
2009-10-14 16:52:26 -0400

    Adding new users to daily email summary

--- app/models/daily_report.rb
+++ app/models/daily_report.rb
@@ -7,11 +7,12 @@ class DailyReport < ActionMailer::Base
     datetime = date.to_datetime
     beginning_of_date_integer = datetime.beginning_of_day.to_time.to_i
     end_of_day_integer = datetime.end_of_day.to_time.to_i
+    todays_users = User.find(:all, :conditions => {:add_date => beginning_of_date_integer..end_of_day_integer})
     todays_groups = Group.find(:all, :conditions => {:register_time => beginning_of_date_integer..end_of_day_integer})
     todays_forum_messages = Forum.find(:all, :conditions => {:post_date => beginning_of_date_integer..end_of_day_integer}).reject {|m| m.posted_by.user_id == 102}
     todays_opened_artifacts = Artifact.find(:all, :conditions => {:open_date => beginning_of_date_integer..end_of_day_integer})
     todays_released_files = FrsFile.find(:all, :conditions => {:post_date => beginning_of_date_integer..end_of_day_integer})
-    body :date => date, :todays_groups => todays_groups, :todays_forum_messages => todays_forum_messages, :todays_opened_artifacts => todays_opened_artifacts, :todays_released_files => todays_released_files
+    body :date => date, :todays_users => todays_users, :todays_groups => todays_groups, :todays_forum_messages => todays_forum_messages, :todays_opened_artifacts => todays_opened_artifacts, :todays_released_files => todays_released_files
   end
   
 end
